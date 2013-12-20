afa73de59790d14ff8f69641f73c80102b71d6bc
Christine Rondeau <christine@bluelimemedia.com>
2013-12-20 10:57:35 -0800

    Slight adjustment to title tag to allow for nice titles on pages.

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
 	<meta charset="<?php bloginfo('charset'); ?>" />
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-	<title><?php wp_title(); ?></title>
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	
