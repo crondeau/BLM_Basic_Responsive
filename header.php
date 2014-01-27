22bf60a8e77d9ff1a783c894f2d6cff8129f928f
Christine Rondeau <christine@bluelimemedia.com>
2014-01-27 09:32:18 -0800

    Deleted custom header
    Pulled code for custom header and added it to its own repo.
    (https://github.com/crondeau/WP-custom-header)
    I never use this and I constantly have to pull it out.

--- header.php
+++ header.php
@@ -48,14 +48,6 @@
 			<h2 id="tagline"><?php bloginfo( 'description' ); ?></h2>
 			<?php get_template_part( 'inc/socmed' ); ?>
 
-	<!-- if there's a header image, let's display it here 
-		 if you don't want to use the header image, just delete lines 46 to 52-->
-	<?php $header_image = get_header_image();
-	if ( ! empty( $header_image ) ) { ?>
-
-		<a href="<?php echo home_url() ?>/"><img class="hide-for-small" src="<?php header_image(); ?>" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" alt="" /></a>
-
-	<?php } ?>
 		
 	<nav id="site-navigation" class="main-navigation" role="navigation">
 		<h1 class="menu-toggle"><?php _e( 'Menu', 'blm_basic' ); ?></h1>
