74433a0ee1625af598f0b9fb04ac3b33a6551657
Christine Rondeau <christine@bluelimemedia.com>
2014-04-25 12:43:38 -0700

    Added class to headings in blog h2 a. This allows us to target specifically instead of just using h2 a.

--- archive.php
+++ archive.php
@@ -47,7 +47,7 @@ get_header(); ?>
 			
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 			
-	    	<h2><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+	    	<h2 class="post-title"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 	
 		 	<?php the_content( __( 'Read more', 'blm_basic' ) );?>
 				
