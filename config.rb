f1590a312af6047b2cfc9fb9b8f00e289bb3dd68
Christine Rondeau <christine@bluelimemedia.com>
2014-05-01 12:51:32 -0700

    Updated the config file for Sass.

--- config.rb
+++ config.rb
@@ -1,55 +1,21 @@
-##
-## This file is only needed for Compass/Sass integration. If you are not using
-## Compass, you may safely ignore or delete this file.
-##
-## If you'd like to learn more about Sass and Compass, see the sass/README.txt
-## file for more information.
-##
+# Require any additional compass plugins here.
 
-# Change this to :production when ready to deploy the CSS to the live server.
-environment = :development
-# environment = :production
-
-# In development, we can turn on the FireSass-compatible debug info.
-firesass = false
-#firesass = true 
-
-# Location of the theme's resources.
-css_dir         = "css"
-sass_dir        = "sass"
-extensions_dir  = "sass-extensions"
-images_dir      = "images"
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
 javascripts_dir = "js"
 fonts_dir       = "font"
 
-# Require any additional compass plugins installed on your system.
-# require 'rgbapng'
-# require 'susy' 
-# require 'stitch'
-# require 'compass-normalize'
-
-##
-## You probably don't need to edit anything below this.
-##
-
-# You can select your preferred output style here (can be overridden via the
-# command line):
+# You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = (environment == :development) ? :expanded : :compressed
+output_style = :expanded
 
-# To enable relative paths to assets via compass helper functions. Since Drupal
-# themes can be installed in multiple locations, we don't need to worry about
-# the absolute path to the theme from the server omega.
-relative_assets = true
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
 
-# To disable debugging comments that display the original location of your
-# selectors. Uncomment:
-# line_comments = false
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
 
-# Pass options to sass. For development, we turn on the FireSass-compatible
-# debug_info if the firesass config variable above is true.
-sass_options = (environment == :development && firesass == true) ? {:debug_info => true} : {}
 
-# Add the root partials folder as import path so we don't have to build the
-# relative paths.
-add_import_path "sass"
