4a582da6a3b5a082cac8ddf33c82dd4bc28f5277
Christine Rondeau <christine@bluelimemedia.com>
2014-05-20 10:19:33 -0700

    Broke the search form in a previous commit.  All fixed now.

--- searchform.php
+++ searchform.php
@@ -8,7 +8,7 @@
 
 <form method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
 	<label for="s" class="screen-reader-text">Search for:</label>
-	<input type="search" class="search-field" placeholder="<?php esc_attr_e('Search', 'blm_basic'); ?>" />
+	<input type="search" class="search-field" name="s" id="s" placeholder="<?php esc_attr_e('Search', 'blm_basic'); ?>" />
 	<button type="submit" class="submit" id="searchsubmit" value="<?php esc_attr_e('Go', 'blm_basic'); ?>"><?php esc_attr_e('Go', 'blm_basic'); ?></button>
 </form>
  
