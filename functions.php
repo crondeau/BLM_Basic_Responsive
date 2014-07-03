77962fbceb3d9edd0a56c450cfc8046e8d2b958c
Christine Rondeau <christine@bluelimemedia.com>
2014-07-03 15:54:01 -0700

    Updated and re-wrote the entire functions.php based on _s. .

--- functions.php
+++ functions.php
@@ -1,68 +1,66 @@
 <?php
 /**
- * blm_basic functions and definitions
+ * BLM Basic Starter Theme functions and definitions
  *
  * @package blm_basic
  */
 
 
-add_action( 'after_setup_theme', 'blm_theme_setup' );
+/**
+ * Set the content width based on the theme's design and stylesheet.
+ */
+if ( ! isset( $content_width ) ) {
+	$content_width = 640; /* pixels */
+}
+
+if ( ! function_exists( 'blm_theme_setup' ) ) :
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ */
 function blm_theme_setup() {
 
-	global $content_width;
-	/* Set the $content_width for things such as video embeds. */
-	if ( !isset( $content_width ) )
-	$content_width = 640;
 
-	// Add theme support for automatic feed links. 
+	// Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
 
 	// Add theme support for post thumbnails (featured images). 
 	add_theme_support( 'post-thumbnails' );
+
+	// Add theme support for menus
+	register_nav_menus( array(
+		'primary' => __( 'Primary Menu', 'blm_basic' ),
+	) );
 	
 	// Enable support for HTML5 markup.
 	add_theme_support( 'html5', array(
-		'comment-list',
-		'search-form',
-		'comment-form',
-		'gallery',
-		'caption',
+		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
 	) );
-	
+
 }
-
-/* Add your nav menus function to the 'init' action hook. */
-   add_action( 'init', 'blm_register_menus' );
-
-/* Add custom actions. */
-   add_action( 'widgets_init', 'blm_register_sidebars' );
+endif; // blm_theme_setup
+add_action( 'after_setup_theme', 'blm_theme_setup' );
 
 
-// Add menu features 
-function blm_register_menus() {
-	register_nav_menus(array('primary'=>__( 'Primary Menu' ), ));
+/**
+ * Register widget area
+ */
+
+function blm_widgets_init() {
+	register_sidebar( array(
+		'id' => 'primary',
+		'name' => __( 'Primary Sidebar', 'blm_basic' ),
+		'description' => __( 'The following widgets will appear in the main sidebar div.', 'blm_basic' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h4>',
+		'after_title' => '</h4>'
+	) );
 }
-
-// Get our wp_nav_menu() fallback, wp_page_menu(), to show a home link.
-function blm_page_menu_args( $args ) {
-	$args['show_home'] = true;
-	return $args;
-}
-add_filter( 'wp_page_menu_args', 'blm_page_menu_args' );
-
-function blm_register_sidebars() {
-	register_sidebar(
-		array(
-			'id' => 'primary',
-			'name' => __( 'Primary Sidebar', 'blm_basic' ),
-			'description' => __( 'The following widgets will appear in the main sidebar div.', 'blm_basic' ),
-			'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-			'after_widget' => '</aside>',
-			'before_title' => '<h4>',
-			'after_title' => '</h4>'
-		)
-	);
-}
+add_action( 'widgets_init', 'blm_widgets_init' );
 
 /**
  * Enqueue scripts and styles
@@ -79,14 +77,13 @@ function blm_basic_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
-
-	if ( is_singular() && wp_attachment_is_image() ) {
-		wp_enqueue_script( 'keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
-	}
 }
 add_action( 'wp_enqueue_scripts', 'blm_basic_scripts' );
 
-//Set up title if SEO plugin is not used.
+
+/**
+ * Set up title if SEO plugin is not used
+ */
 
 function blm_basic_wp_title( $title, $sep ) {
 	global $paged, $page;
